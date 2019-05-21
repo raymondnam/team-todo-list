d741599c11040c44a7b3b9bb54715158d180d528
Raymond Nam <raymond.nam.pro@gmail.com>
2019-05-21 19:39:21 +0100

    setup redux, storybook, styled-components

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,10 @@
+import actionTypes from 'constants/actionTypes';
+
+const initialState = {};
+
+export default function ideaReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
